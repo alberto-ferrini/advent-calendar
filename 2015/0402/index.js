1314242218a994ae33b0a69ce714f8c912d062c1
alberto-ferrini <alberto.ferrini.mail@gmail.com>
2024-12-04 16:06:17 +0100

    Added 2015/0402

--- 2015/0402/index.js
+++ 2015/0402/index.js
@@ -3,8 +3,8 @@ import { MD5 } from "../../md5.js";
 
 const input = readInput();
 let i = 0;
-while (!MD5(input + i).startsWith("00000")) {
+while (!MD5(input + i).startsWith("000000")) {
   i++;
 }
 
-checkRightAnswer(i, 346386);
+checkRightAnswer(i, 9958218);
